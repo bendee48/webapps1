4ed94e23d6bffe0a3a0efbc504509e40557894cb
bendee48 <benbanj@hotmail.com>
2019-01-21 22:25:56 +0000

    Initial commit

---
+++ bin/app.rb
@@ -0,0 +1,76 @@
+require 'sinatra'
+
+set :port, 8080
+set :static, true
+set :public_folder, "static"
+set :views, "views"
+
+get '/' do
+    return 'Hello World'
+end
+
+get '/hello/' do
+    erb :hello_form
+    #greeting = params[:greeting] || "Hi There"
+    #erb :index, :locals => {'greeting' => greeting}
+end
+
+post '/hello/' do
+  greeting = params[:greeting] || "Hi there"
+  name = params[:name] || 'Nobody'
+
+  erb :index, :locals => {'greeting' => greeting, 'name' => name}
+end
+
+get '/biteme/:name' do |x|
+  "Bite me #{x}."
+end
+
+get '/splatty/*.*' do
+  params['splat'][0]
+end
+
+get '/hola' do
+  erb :hola
+end
+
+get '/looky/:name' do
+  params.inspect
+end
+
+get '/passing/' do
+  @name = 'Ben'
+  erb :passing
+end
+
+get '/comment/' do
+  erb :comment
+end
+
+post '/comment/' do
+  @comment = params[:comment]
+  erb :comment_post
+end
+
+get '/names/' do
+  erb :names
+end
+
+def store_name(filename, string)
+  File.open('static/' + filename, 'a+') do |file|
+    file.puts(string)
+  end
+end
+
+def read_names(filename)
+  return [] unless File.exist?("static/" + filename)
+  File.read("static/" + filename).split("\n")
+end
+
+post '/names/' do
+  @name = params[:name]
+  @file = "names.txt"
+  store_name(@file, @name)
+  @name_list = read_names(@file)
+  erb :names_thanks
+end
